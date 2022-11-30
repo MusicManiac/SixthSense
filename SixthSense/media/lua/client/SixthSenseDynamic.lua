42c9d1c90ce1aa2006386b389dd14acf52323e06
Tchernobill <Katupia@hotmail.com>
2022-11-30 05:23:40 +0100

    1.3

--- SixthSense/media/lua/client/SixthSenseDynamic.lua
+++ SixthSense/media/lua/client/SixthSenseDynamic.lua
@@ -1,7 +1,7 @@
 function SixthSenseDynamic()
 	for playerIndex = 0, getNumActivePlayers() - 1 do
 		local player = getSpecificPlayer(playerIndex);
-		if not player:HasTrait("SixthSense") and (player:getPerkLevel(Perks.Lightfoot) + player:getPerkLevel(Perks.Nimble)) >= SandboxVars.SixthSense.SixthSenseLevelsRequired and player:getHoursSurvived() >= (SandboxVars.SixthSense.SixthSenseDaysSurvived * 24) and player:getZombieKills() >= SandboxVars.SixthSense.SixthSenseKillsRequired then
+		if not player:HasTrait("SixthSense") and (player:getPerkLevel(Perks.Lightfoot) + player:getPerkLevel(Perks.Nimble)) >= SandboxVars.Moodles.SixthSenseLevelsRequired and player:getHoursSurvived() >= (SandboxVars.Moodles.SixthSenseDaysSurvived * 24) and player:getZombieKills() >= SandboxVars.Moodles.SixthSenseKillsRequired then
 			player:getTraits():add("SixthSense");
 			HaloTextHelper.addTextWithArrow(player, getText("UI_trait_SixthSense"), true, HaloTextHelper.getColorGreen());
 		end
@@ -9,14 +9,14 @@ function SixthSenseDynamic()
 end
 
 function SixthSenseDynamicInit()
-	if SandboxVars.SixthSense.SixthSenseDynamic == true then
-		if SandboxVars.SixthSense.SixthSenseLevelsRequired >= 1 then
+	if SandboxVars.Moodles.SixthSenseDynamic == true then
+		if SandboxVars.Moodles.SixthSenseLevelsRequired >= 1 then
 			Events.LevelPerk.Add(SixthSenseDynamic);
 		end
-		if SandboxVars.SixthSense.SixthSenseDaysSurvived >= 1 then
+		if SandboxVars.Moodles.SixthSenseDaysSurvived >= 1 then
 			Events.EveryDays.Add(SixthSenseDynamic);
 		end
-		if SandboxVars.SixthSense.SixthSenseKillsRequired >= 1 then
+		if SandboxVars.Moodles.SixthSenseKillsRequired >= 1 then
 			Events.OnZombieDead.Add(SixthSenseDynamic);
 		end
 	end
